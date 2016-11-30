2c112357240abb234429ea84b528ac0c77d52fa2
Ramon Tayag <ramon.tayag@gmail.com>
2016-11-30 08:31:44 -0800

    Add beginnings of API

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
     resources :txns, only: %i[new create show]
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :txns, only: %i[update]
+    end
+  end
+
 end
