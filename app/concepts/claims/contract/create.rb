f4b6d96686eedeb8084282dc0ff7c313d6b4a2ff
Ramon Tayag <ramon.tayag@gmail.com>
2017-03-31 17:17:25 +0800

    Remove `recipient_first_name` and `recipient_last_name` from Claim contract

--- app/concepts/claims/contract/create.rb
+++ app/concepts/claims/contract/create.rb
@@ -2,8 +2,6 @@ module Claims
   module Contract
     class Create < ApplicationContract
 
-      property :recipient_first_name
-      property :recipient_last_name
       property :ref_no
 
     end
