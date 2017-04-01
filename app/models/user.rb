8dbabfa109a749c4e9011a67c226efc914d640b3
Ramon Tayag <ramon.tayag@gmail.com>
2017-04-01 10:22:46 +0800

    Send user mail async

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,11 @@ class User < ApplicationRecord
     :registerable,
   )
 
+  # Enable async delivery of mail
+  # https://github.com/plataformatec/devise#activejob-integration
+  # Too bad it's in the model
+  def send_devise_notification(notification, *args)
+    devise_mailer.send(notification, self, *args).deliver_later
+  end
+
 end
