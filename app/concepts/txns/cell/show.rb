556efc8449aeca4c2c32b5e6db9c2a1f68e453f3
Ramon Tayag <ramon.tayag@gmail.com>
2017-04-05 14:24:12 +0800

    Tweak txn show page
    Use table like claims

--- app/concepts/txns/cell/show.rb
+++ app/concepts/txns/cell/show.rb
@@ -4,6 +4,20 @@ module Txns
 
       property :ref_no
       property :status
+      property :sender_first_name
+      property :sender_last_name
+      property :sender_mobile
+      property :sender_street
+      property :sender_city
+      property :sender_province
+      property :sender_region
+      property :recipient_first_name
+      property :recipient_last_name
+      property :recipient_mobile
+      property :recipient_street
+      property :recipient_city
+      property :recipient_province
+      property :recipient_region
 
       def row
         render :row
