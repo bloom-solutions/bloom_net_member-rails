1b9707dd403a31dc853d69544d4b2086abe161ec
Ramon Tayag <ramon.tayag@gmail.com>
2017-04-06 09:06:12 +0800

    Fix redirection errors after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    request.referrer || dashboard_path
+    if resource.is_a?(User)
+      dashboard_path
+    elsif resource.is_a?(AdminUser)
+      admin_path
+    end
   end
 
 end
